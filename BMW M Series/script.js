7817cd60ddbc75b7cfb4bc154e1449fced4167d3
Tikesh115 <150375340+Tikesh115@users.noreply.github.com>
2024-11-23 22:39:57 +0530

    Add files via upload

--- BMW M Series/script.js
+++ BMW M Series/script.js
@@ -3,3 +3,24 @@ document.getElementById("clickText").addEventListener("click", function() {
     sound.play();
 });
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "flex";  
+    setTimeout(showSlides, 5000); // Change image every 4 seconds
+}
+
+function plusSlides(n) {
+    slideIndex += n;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    if (slideIndex < 1) {slideIndex = slides.length}
+    showSlides();
+}
