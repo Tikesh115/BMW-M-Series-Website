65e092054a43fba5a3386afeb9153ed9c348b9e5
Tikesh115 <150375340+Tikesh115@users.noreply.github.com>
2024-11-24 23:02:08 +0530

    Add files via upload

--- BMW M Series/script.js
+++ BMW M Series/script.js
@@ -1,8 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", reveal);
+});
+
 document.getElementById("clickText").addEventListener("click", function() {
     var sound = document.getElementById("sound");
     sound.play();
 });
 
+// slideing-transistion-start
 let slideIndex = 0;
 showSlides();
 
@@ -15,7 +20,7 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "flex";  
-    setTimeout(showSlides, 5000); // Change image every 4 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
 function plusSlides(n) {
@@ -23,4 +28,23 @@ function plusSlides(n) {
     if (slideIndex > slides.length) {slideIndex = 1}
     if (slideIndex < 1) {slideIndex = slides.length}
     showSlides();
+}
+// slideing-transistion-end
+
+// scroll-reveal-animation
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+
+    for(var i = 0; i < reveals.length; i++) {
+        var revealHeight = window.innerHeight - 150;
+        var revealTop = reveals[i].getBoundingClientRect().top;  //will return the top position relative to viewport.
+        
+        if(revealTop < revealHeight) {
+            reveals[i].classList.add("active");
+        }
+        else {
+            reveals[i].classList.remove("active");
+        }
+    }
 }
